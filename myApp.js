389051c74322325a93f02a43dbd812863f29fc8c
ex15263 <daniel.savin@digipolis.be>
2023-12-12 22:04:24 +0100

    Done from challange 1 to 9, out of 14.

--- myApp.js
+++ myApp.js
@@ -2,7 +2,16 @@ const express = require('express');
 const helmet = require('helmet')
 const app = express();
 
-helmet.hidePoweredBy();
+ninetyDaysInSeconds = 90*24*60*60;
+
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({action: 'deny'}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({maxAge:ninetyDaysInSeconds, force:true}));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
 
 
 
